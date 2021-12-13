ffdc0969c3c45e6616315a6711474e2aa88607b7
fuchao <chao.fu@yswy.vip>
2021-12-13 23:43:26 +0800

    1

--- src/main/resources/static/rarity/js/chain.js
+++ src/main/resources/static/rarity/js/chain.js
@@ -1,7 +1,6 @@
 $(function () {
 	$.summon = function(_name, _race, _class, _gender, _ref) {
 		$.tips("Summoning...");
-		$.showOverlay();
 		$.mainContract.methods
 			.summon(_name, _race, _class, _gender, _ref)
 			.send({from: $.walletAddress}, $.errorProcess)
@@ -13,11 +12,28 @@ $(function () {
 			});
 	};
 	
-	$.summoner = async function (tokenId, price, summoners) {
-		let result = await $.mainContract.methods.summoner(tokenId).call();
+	$.approve = function() {
+		$.tips("Approve...");
+		$.erc20Contract.methods
+			.approve($.MARKET_ADDR, $.toWei("100000000"))
+			.send({from: $.walletAddress}, $.errorProcess)
+			.then((tx) => {
+				$.resultProcess(tx, function(){
+					$.tips("Success", 2000);
+					$("#goods_body").find(".btn_buy").removeAttr("disabled");
+					$("#buy_approve").hide();
+				});
+			});
+	};
+	
+	$.summoner = async function (goods, summoners) {
+		// query
+		let result = await $.mainContract.methods.summoner(goods.tokenId).call();
 		var obj = {
-			tokenId: tokenId,
-			price: $.fromWei(price, null, 18),
+			tokenId: goods.tokenId,
+			price: $.fromWei(goods.price),
+			status: goods.status,
+			my: (goods.status == 0 && goods.owner.toLowerCase() == $.walletAddress) ? true : false,
 			name: result[0],
 			clazz: result[1],
 			race: result[2],
@@ -27,8 +43,18 @@ $(function () {
 			adventurersLog: Number(result[6])
 		};
 		summoners.push(obj);
+		
+		// show & bind event
 		$("#goods_body").setTemplateElement("goods_template");
 		$("#goods_body").processTemplate(summoners);
+		$.isApprovedErc20();
+		$("#goods_body").find(".btn_buy").each(function() {
+			var _this = $(this);
+			_this.on('click', function(){
+				var tokenId = _this.attr("tokenId");
+				$.buy(tokenId);
+			});
+		});
 	};
 	
 	$.getGoodsPage = async function(pageIdx, pageSize) {
@@ -37,48 +63,70 @@ $(function () {
 		let result = await $.marketContract.methods.getGoodsPage(pageIdx, pageSize).call();
 		var summoners = [];
 		for (goods of result) {
-			$.summoner(goods.tokenId, goods.price, summoners);
+			$.summoner(goods, summoners);
 		}
 	};
 	
-	$.isApprovedForAll = async function(owner, operator) {
-		let result = await $.mainContract.methods.isApprovedForAll(owner, operator).call();
+	$.isApprovedForAll = async function() {
+		let result = await $.mainContract.methods.isApprovedForAll($.walletAddress, $.MARKET_ADDR).call();
 		if (result) {
 			$("#open_list").show();
-			$("#approve").hide();
+			$("#sell_approve").hide();
 		} else {
-			$("#approve").show();
+			$("#sell_approve").show();
 			$("#open_list").hide();
 		}
 	};
 	
-	$.setApprovalForAll = function (operator) {
+	$.isApprovedErc20 = async function() {
+		let result = await $.erc20Contract.methods.allowance($.walletAddress, $.MARKET_ADDR).call();
+		var allowance = Number($.fromWei(result[0]));
+		if (allowance > 0) {
+			$("#goods_body").find(".btn_buy").removeAttr("disabled");
+			$("#buy_approve").hide();
+		} else {
+			$("#goods_body").find(".btn_buy").attr("disabled", "disabled");
+			$("#buy_approve").show();
+		}
+	};
+	
+	$.setApprovalForAll = function () {
 		$.tips("Approve...");
-		$.showOverlay();
 		$.mainContract.methods
-			.setApprovalForAll(operator, true)
+			.setApprovalForAll($.MARKET_ADDR, true)
 			.send({from: $.walletAddress}, $.errorProcess)
 			.then((tx) => {
 				$.resultProcess(tx, function() {
-					$.hideTips();
-					$.hidePopup();
-					$("#approve").hide();
-					$("#list").show();
+					$.tips("Success", 2000);
+					$("#sell_approve").hide();
+					$("#open_list").show();
 				});
 			});
 	};
 	
 	$.list = function(tokenId, price) {
 		$.tips("List...");
-		$.showOverlay();
-		price = $.toWei(price, null, 18);
+		price = $.toWei(price);
 		$.marketContract.methods
 			.list(tokenId, price)
 			.send({from: $.walletAddress}, $.errorProcess)
 			.then((tx) => {
 				$.resultProcess(tx, function() {
-					$.hideTips();
+					$.tips("Success", 2000);
 					$.hidePopup();
+					$.getGoodsPage($.pageIdx, $.pageSize);
+				});
+			});
+	};
+	
+	$.buy = function(tokenId) {
+		$.tips("Buy...");
+		$.marketContract.methods
+			.buy(tokenId)
+			.send({from: $.walletAddress}, $.errorProcess)
+			.then((tx) => {
+				$.resultProcess(tx, function() {
+					$.tips("Success", 2000);
 					$.getGoodsPage($.pageIdx, $.pageSize);
 				});
 			});
